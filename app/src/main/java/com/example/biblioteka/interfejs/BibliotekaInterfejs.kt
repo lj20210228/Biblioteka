309672d68ffd0bbd1a12f1f401d919cded73dd90
lazar-jankovic <lazarj1007@gmail.com>
2025-09-01 15:54:56 +0200

    Dodat interfejs BibliotekaInterfejs.kt

---
+++ app/src/main/java/com/example/biblioteka/interfejs/BibliotekaInterfejs.kt
@@ -0,0 +1,13 @@
+package com.example.biblioteka.interfejs
+
+import com.example.biblioteka.Autor
+import com.example.biblioteka.Knjiga
+import java.time.LocalDateTime
+
+interface BibliotekaInterfejs {
+
+    fun dodajKnjigu(knjiga: Knjiga): LocalDateTime
+    fun obrisiKnjigu(knjiga: Knjiga): LocalDateTime
+    fun vratiSveKnjige(): List<Knjiga>
+    fun pronadjiKnjigu(autor: Autor,isbn: Long,naslov: String,izdavac: String): List<Knjiga>
+}
